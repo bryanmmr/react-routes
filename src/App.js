0e31da2479a4b24bdb7989bd032b992ba26a519d
bryanmmr <bryanmmr@gmail.com>
2021-12-01 11:36:59 -0500

    add: deploy gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Price from './components/Price';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='navigator'>
+        <Link to='/home'>Home</Link>
+        <Link to='/price'>Price</Link>
+        <Link to='/about'>About</Link>
+      </nav>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/price" element={<Price />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
